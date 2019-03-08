ef556668daeb4123e74807fcfe38afdb3fe47235
Steven Avelino <steven.avelino24@outlook.com>
2019-03-08 17:07:57 +0100

    Get list of customers with last name selected

--- wwwroot/js/ContractCreate.js
+++ wwwroot/js/ContractCreate.js
@@ -1,3 +1,15 @@
 /**
  * File for Contracts form
  */
+
+document.getElementById('LastNames').onchange = async () => {
+    try {
+        const response = await fetch(`https://localhost:5001/api/names-list/${document.getElementById('LastNames').value}`, { 
+            headers: { "Content-Type": "application/json" }
+          })
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
