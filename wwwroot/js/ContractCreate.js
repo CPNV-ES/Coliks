b9588f5521fd443468a1d76ab1d504873b73d56b
Steven Avelino <steven.avelino24@outlook.com>
2019-03-09 09:45:59 +0100

    Add condition for phone number display

--- wwwroot/js/ContractCreate.js
+++ wwwroot/js/ContractCreate.js
@@ -13,7 +13,7 @@ getCustomerContracts = async (id) => {
 }
 
 setCustomerInfo = async () => {
-    document.getElementById('Phone').value = customers[0].phone
+    document.getElementById('Phone').value = customers[0].phone !== null ? customers[0].phone : 'Non défini'
     document.getElementById('Address').value = customers[0].address !== null ? customers[0].address : 'Non définie'
     contracts = await getCustomerContracts(customers[0].id)
 }
@@ -60,7 +60,7 @@ document.getElementById('FirstName').onchange = async () => {
         if (customer.id === Number(document.getElementById('FirstName').value)) selectedCustomer = customer
     }
 
-    document.getElementById('Phone').value = selectedCustomer.phone
+    document.getElementById('Phone').value = selectedCustomer.phone !== null ? selectedCustomer.phone : 'Non défini'
     document.getElementById('Address').value = selectedCustomer.address !== null ? selectedCustomer.address : 'Non définie'
     contracts = await getCustomerContracts(Number(document.getElementById('FirstName').value))
 }
