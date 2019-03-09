c9482024b46d135dd1fce2ed6579e7326e341e61
Steven Avelino <steven.avelino24@outlook.com>
2019-03-09 09:00:21 +0100

    Create list of first names options

--- wwwroot/js/ContractCreate.js
+++ wwwroot/js/ContractCreate.js
@@ -16,10 +16,18 @@ document.getElementById('LastNames').onchange = async () => {
             if (customers.length === 1) {
                 let optionFirstName = document.createElement('option')
                 optionFirstName.text = `${customers[0].firstname} (${customers[0].phone})`
-                optionFirstName.value = customers[0].phone
+                optionFirstName.value = customers[0].id
                 document.getElementById('FirstName').add(optionFirstName)
                 document.getElementById('Phone').value = customers[0].phone
                 document.getElementById('Address').value = customers[0].address !== null ? customers[0].address : 'Non définie'
+            } else {
+                for (const customer of customers) {
+                    let optionTemp = document.createElement('option')
+                    optionTemp.text = `${customer.firstname} (${customer.phone})`
+                    optionTemp.value = customer.id
+                    document.getElementById('FirstName').add(optionTemp)
+                    document.getElementById('FirstName').disabled = false
+                }
             }
         }
     } catch (error) {
