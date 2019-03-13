b41345cf6689aeb28043230d56a0351a56b0c627
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-03-13 19:56:29 +0100

    override model name fields with french names

--- Models/Customers.cs
+++ Models/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace coliks.Models
 {
@@ -12,14 +13,21 @@ namespace coliks.Models
         }
 
         public int Id { get; set; }
+        [DisplayName("Nom")]
         public string Lastname { get; set; }
+        [DisplayName("Prenom")]
         public string Firstname { get; set; }
+        [DisplayName("Adresse")]
         public string Address { get; set; }
         public int? CityId { get; set; }
+        [DisplayName("Téléphone")]
         public string Phone { get; set; }
+        [DisplayName("Email")]
         public string Email { get; set; }
+        [DisplayName("Mobile")]
         public string Mobile { get; set; }
 
+        [DisplayName("Ville")]
         public virtual Cities City { get; set; }
         public virtual ICollection<Contracts> Contracts { get; set; }
         public virtual ICollection<Purchases> Purchases { get; set; }
