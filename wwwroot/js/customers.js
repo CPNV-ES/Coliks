ee7a95a4f412615fda32ba4cd118c204edaa1685
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-03-15 19:19:01 +0100

    add row-clickable function for customers list

--- wwwroot/js/customers.js
+++ wwwroot/js/customers.js
@@ -1,1 +1,36 @@
-﻿
+﻿/**
+ * Entry point
+ */
+$(document).ready(function () {
+    htmlPage.init();
+});
+
+
+/**
+ *Home page events
+ * */
+var htmlPage = {
+    init: function () {
+        this.loadEvents();
+    },
+    loadEvents: function () {
+        $(".customer-clickable-row .clickable").on("click", function (e) {
+            e.preventDefault();
+            customer.details($(this));
+        })
+    },
+}
+
+
+/**
+ * Customers base functions
+ * */
+var customer = {    
+    details: function (elm) {
+        window.location = this.getDetailsRoute(elm);
+    },
+    getDetailsRoute: function (elm) {
+        return $(elm).parent().find('#customer-home-actions a:nth-child(2)').attr('href');
+    }
+}
+
