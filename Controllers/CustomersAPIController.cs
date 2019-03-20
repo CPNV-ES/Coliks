fac3f48231ec259d9d2a06facc2324163385f9d2
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-03-21 00:51:12 +0100

    add Customer API controller

---
+++ Controllers/CustomersAPIController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using coliks.Models;
+
+namespace coliks
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersAPIController : ControllerBase
+    {
+        private readonly ColiksContext _context;
+
+        public CustomersAPIController(ColiksContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CustomersAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customers>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET: api/CustomersAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customers>> GetCustomers(int id)
+        {
+            var customers = await _context.Customers.FindAsync(id);
+
+            if (customers == null)
+            {
+                return NotFound();
+            }
+
+            return customers;
+        }
+
+        // PUT: api/CustomersAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomers(int id, Customers customers)
+        {
+            if (id != customers.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CustomersAPI
+        [HttpPost]
+        public async Task<ActionResult<Customers>> PostCustomers(Customers customers)
+        {
+            _context.Customers.Add(customers);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomers", new { id = customers.Id }, customers);
+        }
+
+        // DELETE: api/CustomersAPI/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Customers>> DeleteCustomers(int id)
+        {
+            var customers = await _context.Customers.FindAsync(id);
+            if (customers == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customers);
+            await _context.SaveChangesAsync();
+
+            return customers;
+        }
+
+        private bool CustomersExists(int id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
