cf35a28e9bc1d03feaf2fa77a7ef5263ab8c0cb8
Steven Avelino <steven.avelino24@outlook.com>
2019-03-20 08:57:26 +0100

    Add API endpoint to get price of item

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using coliks.Models;
+using Newtonsoft.Json.Linq;
 
 namespace coliks.Controllers
 {
@@ -65,6 +66,13 @@ namespace coliks.Controllers
         public async Task<ActionResult<List<Durations>>> GetDurations()
         {
             return await _context.Durations.ToListAsync();
+        }
+
+        [HttpGet("/api/get-price")]
+        public async Task<ActionResult<Rentprices>> GetPriceItem(int CategoryId, String ItemType, int DurationId)
+        {
+            var gearType = await _context.Geartypes.FirstOrDefaultAsync(gt => gt.Name == ItemType);
+            return await _context.Rentprices.Where(rt => rt.CategoryId == CategoryId && rt.DurationId == DurationId && rt.GeartypeId == gearType.Id).FirstOrDefaultAsync();
         }
     }
 }
