065cd09eeae8dd80c500986f349799503ecdbfb8
Davide CARBONI <Davide.CARBONI@cpnv.ch>
2019-03-21 15:07:28 +0100

    add validation in purchase model

--- Models/PartialClasses/Purchases.cs
+++ Models/PartialClasses/Purchases.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace coliks.Models
 {
@@ -9,8 +10,10 @@ namespace coliks.Models
         [DisplayName("Date")]
         public DateTime? Date { get; set; }
         [DisplayName("Description")]
+        [Required(ErrorMessage = "La description est obligatoire")]
         public string Description { get; set; }
         [DisplayName("Total")]
+        [Required(ErrorMessage = "Le montant est vide")]
         public double? Amount { get; set; }
     }
 }
