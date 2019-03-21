c584e23b91da95574bfc3ccd19bbdf0248490964
Davide CARBONI <Davide.CARBONI@cpnv.ch>
2019-03-21 14:50:49 +0100

    add partial class for purchase model

---
+++ Models/PartialClasses/Purchases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace coliks.Models
+{
+    public partial class Purchases
+    {
+        [DisplayName("Date")]
+        public DateTime? Date { get; set; }
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        [DisplayName("Total")]
+        public double? Amount { get; set; }
+    }
+}
--- Models/Purchases.cs
+++ Models/Purchases.cs
@@ -8,13 +8,7 @@ namespace coliks.Models
     {
         public int Id { get; set; }
         public int? CustomerId { get; set; }
-        [DisplayName("Date")]
-        public DateTime? Date { get; set; }
-        [DisplayName("Description")]
-        public string Description { get; set; }
-        [DisplayName("Total")]
-        public double? Amount { get; set; }
-
+        
         public virtual Customers Customer { get; set; }
     }
 }
