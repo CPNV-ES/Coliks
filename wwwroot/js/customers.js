0625c6e3ff35439a598be9db045bb220b9669c0d
Davide CARBONI <Davide.CARBONI@cpnv.ch>
2019-03-21 11:58:39 +0100

    delete functions unused in home customer

--- wwwroot/js/customers.js
+++ wwwroot/js/customers.js
@@ -1,31 +1,45 @@
 ﻿/**
+ Author: Carboni Davide
+ Description: init all necessary events into Customers home page
+ version: 1.0
+ */
+
+/**
  * Entry point
  */
 $(document).ready(function () {
-    htmlPage.init();
+    customersPage.init();
 });
 
-
 /**
- *Home page events
+ *Customers home page events
  * */
-var htmlPage = {
+var customersPage = {
     init: function () {
-        this.loadEvents();
+        customersPage.loadEvents();
+        
     },
     loadEvents: function () {
+        // load customer details when row is selected
         $(".customer-clickable-row .clickable").on("click", function (e) {
             e.preventDefault();
+            e.stopPropagation();
             customer.details($(this));
         });
 
-        $("#search-values").on("click", function () {
+        // start search when click in search icon
+        $("#search-values").on("click", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
             if ($("#customer-search-box input").val() == "")
                 return;
             $('#customer-search-box').submit();
         });
 
-        $("#delete-search").on("click", function () {
+        // delete string in box search and restore initial search
+        $("#delete-search").on("click", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
             if ($("#customer-search-box input").val() == "")
                 return;
             $("#customer-search-box input").val("");
@@ -38,29 +52,6 @@ var htmlPage = {
 
          /* Popovers enable */
         $('[data-toggle="popover"]').popover({ delay: { show: 1000, hide: 100 }, trigger: "hover" });
-
-        /* Vaucher button */
-
-        $("#purchase-add-vaucher").on("click", function(){
-            //get input data
-            var customerId = $("#customerId").val();
-            var amount = - $("#amount").val();
-            var description = $("#description").val();
-            //send data to api to store new vaucher
-            purchase.add(customerId, amount, description, false);
-        });
-
-        /* Add new Purchase to a customer */
-
-        $("#add-purchase-customer-button").on("click", function () {
-            //get input data
-            var customerId = $("#add-purchase-customer #customerId").val();
-            var amount = $("#add-purchase-customer #amount").val();
-            var description = $("#add-purchase-customer #description").val();
-            //send data to api to store new vaucher
-            purchase.add(customerId, amount, description, true);
-        });
-
     },
 }
 
@@ -73,34 +64,5 @@ var customer = {
     },
     getDetailsRoute: function (elm) {
         return $(elm).parent().find('#customer-home-actions a:nth-child(2)').attr('href');
-    }
-}
-
-/**
- * Purchases functions
- * */
-
-var purchase = {
-    add: function (customerId, amount, description, noMsg) {
-        let current_datetime = new Date()
-        let formatted_date = current_datetime.getFullYear() + "/" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds()
-
-        $.ajax({
-            accepts: "application/json",
-            contentType: "application/json",
-            type: "POST",
-            url: '/api/PurchasesApi',
-            data: JSON.stringify({CustomerId: customerId, Date: formatted_date, Description: description, Amount: amount}),
-        })
-            .done(function () {
-                if (!noMsg) { alert("Le bon d'achat à été rajouter"); }
-                purchase.update(customerId);
-            })
-            .fail(function () {
-                if (!noMsg) alert("Le bon d'achat n'a été pas crée");
-            });
-    },
-    update: function (customerId) {
-        $("#partialView").load("/Customers/DetailsPartial/"+customerId);
     }
 }
