f7cb8bdd7c0405d0bd7d6c1da70e7e5563a937d5
GIORDANO Antonio <Antonio.giordano17@gmail.com>
2019-03-21 11:53:30 +0100

    Use jquery and combine filter with searchbar

--- wwwroot/js/item.js
+++ wwwroot/js/item.js
@@ -1,5 +1,9 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById('searchBar').onkeyup = function () {
-        document.getElementById('formSearch').submit()
-    }
-});
+﻿$(document).ready(function () {
+    $('#tmpSearchBar').keyup(function (e) {
+        $('#searchBar').val($('#tmpSearchBar').val())
+    })
+
+    $('#btnSearch').click(function () {
+        $('#formSearch').submit()
+    })
+})
