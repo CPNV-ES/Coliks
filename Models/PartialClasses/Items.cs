124301b507c5b98e52ef9c25c72f9652ba4fb320
GIORDANO Antonio <Antonio.giordano17@gmail.com>
2019-03-22 14:25:32 +0100

    Add partial model for Items

---
+++ Models/PartialClasses/Items.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coliks.Models
+{
+    [ModelMetadataType(typeof(ItemsMetaData))]
+    public partial class Items
+    {
+    }
+
+    public class ItemsMetaData
+    {
+        public string Itemnb { get; set; }
+
+        [Display(Name = "Marque")]
+        public string Brand { get; set; }
+
+        [Display(Name = "Modèle")]
+        public string Model { get; set; }
+
+        [Display(Name = "Taille")]
+        public int? Size { get; set; }
+
+        [Display(Name = "Coût")]
+        public int? Cost { get; set; }
+
+        [Display(Name = "Retourné")]
+        public int? Returned { get; set; }
+
+        public string Type { get; set; }
+
+        public int? Stock { get; set; }
+
+        [Display(Name = "Numéro de série")]
+        public string Serialnumber { get; set; }
+
+        [Display(Name = "Catégories")]
+        public Categories Category { get; set; }
+    }
+}
