5c0c48de80d6e000b7fa691297fc00416d107ce0
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-03-25 18:13:28 +0100

    restore old purchase model

--- Models/Purchases.cs
+++ Models/Purchases.cs
@@ -8,7 +8,10 @@ namespace coliks.Models
     {
         public int Id { get; set; }
         public int? CustomerId { get; set; }
-        
+        public DateTime? Date { get; set; }
+        public string Description { get; set; }
+        public double? Amount { get; set; }
+
         public virtual Customers Customer { get; set; }
     }
 }
