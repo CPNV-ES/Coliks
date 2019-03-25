341000a1842161d0cedfe7bd309a6329bc6b8edd
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-03-25 18:13:17 +0100

    restore old customer model

--- Models/Customers.cs
+++ Models/Customers.cs
@@ -9,26 +9,24 @@ namespace coliks.Models
 {
     public partial class Customers
     {
-
-        #region constructor
-
         public Customers()
         {
             Contracts = new HashSet<Contracts>();
             Purchases = new HashSet<Purchases>();
         }
 
-        #endregion
-
-        #region fields
-
         public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Address { get; set; }
         public int? CityId { get; set; }
-       
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+        public virtual Cities City { get; set; }
         public virtual ICollection<Contracts> Contracts { get; set; }
         public virtual ICollection<Purchases> Purchases { get; set; }
-        
-        #endregion
     }
 
 }
