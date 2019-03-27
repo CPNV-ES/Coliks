c051075d3edd51b84a1faf7774f186922a79ad82
Steven Avelino <steven.avelino24@outlook.com>
2019-03-27 14:12:43 +0100

    Add Contracts partial class

---
+++ Models/PartialClasses/Contracts.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace coliks.Models
+{
+    public partial class Contracts
+    {
+        
+    }
+
+    public class ContractsMetadata
+    {
+        [DisplayName("Id")]
+        public int Id { get; set; }
+        [DisplayName("Date de création")]
+        public DateTime? Creationdate { get; set; }
+        [DisplayName("Date de retour éffective")]
+        public DateTime? Effectivereturn { get; set; }
+        [DisplayName("Date de retour plannifiée")]
+        public DateTime? Plannedreturn { get; set; }
+        [DisplayName("Notes")]
+        public string Notes { get; set; }
+        [DisplayName("Prix total")]
+        public double? Total { get; set; }
+        [DisplayName("Date d'emprunt")]
+        public DateTime? Takenon { get; set; }
+        [DisplayName("Date de paiement")]
+        public DateTime? Paidon { get; set; }
+        [DisplayName("Assurance")]
+        public byte? Insurance { get; set; }
+        [DisplayName("Aller checher")]
+        public byte? Goget { get; set; }
+        [DisplayName("Client")]
+        public virtual Customers Customer { get; set; }
+        [DisplayName("Staff aidant")]
+        public virtual Staffs HelpStaff { get; set; }
+        [DisplayName("Staff tunant")]
+        public virtual Staffs TuneStaff { get; set; }
+    }
+}
