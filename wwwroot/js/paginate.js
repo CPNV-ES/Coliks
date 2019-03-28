08512b6262f38c3f6ea2a69a0d365c7632db50e9
GIORDANO Antonio <Antonio.giordano17@gmail.com>
2019-03-28 14:58:24 +0100

    Remove dead code

--- wwwroot/js/paginate.js
+++ wwwroot/js/paginate.js
@@ -1,7 +1,6 @@
 ﻿var paginate = function () {
     var that = {};
     var item = {};
-    var TextBox = '';
     var PageNo = 1;
 
 
@@ -15,14 +14,12 @@
             Pagination();
         });
 
-        // every filter text box has the common class as filter-text  
-        // on keyup of the text box we will call the ajax function  
+        // every filters has the common class as filter-text  
+        // on keyup or change of the filters, we will call the ajax function  
         $(document).on('keyup change', '.filter-text', function () {
             
             //setting the page no to 1 thus on any filter change matching data will be shown from page 1  
             PageNo = 1;
-            //setting the text box id on which we will set the focusend  
-            TextBox = $(this).attr('id');
             Pagination();
         })
     }
@@ -30,6 +27,7 @@
     // Common ajax call function to bind the filter data and page number and pass to the Action result of our controller via ajax call  
     var Pagination = function () {
 
+        // All filters controls
         item.itemnb = $('#item_nb').val();
         item.brand = $('#item_brand').val();
         item.model = $('#item_model').val();
@@ -37,6 +35,7 @@
         item.stock = $('#item_stock').val();
         item.category = $('#item_gender').val();
         item.search = $('#search').val();
+
         $.ajax({
             type: "POST",
             url: "/Items/PaginateData",
@@ -45,29 +44,14 @@
             dataType: "html", //here we set datatype as html becausing we are returning partial view  
             success: function (d) {
                 // d will contain the html of partial view  
-                var result = $.parseHTML(d) // Parse string return into html
-
-                $('#tableBody').replaceWith(result[2].childNodes[3]) // Get the tbody from result and display it
-                $('.panel-footer').replaceWith(result[4])
-                //// setting the focus to the textbox  
-                //if (TextBox != '' && TextBox != null) {
-                //    focusToEnd($('#' + TextBox))
-                //    //$('#' + TextBox).focusToEnd();
-                //}
+                var result = $.parseHTML(d) // Parse string returned
+                $('#tableBody').replaceWith(result[2].childNodes[1].childNodes[3]) // Get the tbody from result and display it
+                $('.panel-footer').replaceWith(result[4]) // Get the footer and display it
             },
             error: function (xhr, textStatus, errorThrown) {
 
             }
         });
-    }
-
-    // This function will focus to the end position of the mentioned textbox id  
-    var focusToEnd = function ($) {
-        $.each(function () {
-            var v = $.val();
-            $.focus().val("").val(v);
-        });
-        
     }
 
     that.init = function () {
