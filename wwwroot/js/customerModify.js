891a47a0617554a776d33c063df8d90686a57949
Davide CARBONI <Davide.CARBONI@cpnv.ch>
2019-04-02 15:01:17 +0200

    add customer js validation script

---
+++ wwwroot/js/customerModify.js
@@ -0,0 +1,51 @@
+﻿/**
+ Author: Carboni Davide
+ Description: init all necessary events into Edit Customer Page
+ version: 1.0
+ */
+
+/**
+ * Entry point
+ */
+$(document).ready(function () {
+    customersModifyPage.init();
+});
+
+/**
+ *Customers home page events
+ * */
+var customersModifyPage = {
+    init: function () {
+        customersModifyPage.loadEvents();
+        
+    },
+    loadEvents: function () {
+        // validate the edit form
+        $(".edit-customer-validation").on("change paste keyup click blur", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $("#edit-form-customer").validate();
+            customer.enableButtonSubmit($("#edit-form-customer").valid());
+        });
+    },
+}
+
+/**
+ * Customers base functions
+ * */
+var customer = {    
+    enableButtonSubmit(res) {
+        // enable button if forms is valide
+        if (res) {
+            console.log("valide");
+            // enable
+            $("#edit-form-customer button").removeClass("disabled");
+            $("#edit-form-customer button").removeAttr("disabled");
+        } else {
+            console.log("invalide");
+            // disable
+            $("#edit-form-customer button").addClass("disabled");
+            $("#edit-form-customer button").attr("disabled", "disabled");
+        }
+    }
+}
