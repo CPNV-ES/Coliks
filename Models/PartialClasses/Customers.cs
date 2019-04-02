2cb103e9eb4a6f255fcdb17c9e2e17403ed88adb
Davide CARBONI <Davide.CARBONI@cpnv.ch>
2019-04-02 15:01:38 +0200

    use regex to validate phone number

--- Models/PartialClasses/Customers.cs
+++ Models/PartialClasses/Customers.cs
@@ -20,23 +20,30 @@ namespace coliks.Models
         [StringLength(50, ErrorMessage = "La taille doit être plus petite que 50 caracters")]
         [Remote(action: "VerifyName", controller: "Customers", AdditionalFields = "Firstname, Id", ErrorMessage = "Le client exsiste déja")]
         public string Lastname { get; set; }
+
         [DisplayName("Prenom")]
         [Required(ErrorMessage = "Le prenom du client est obligatoire")]
         [StringLength(50, ErrorMessage = "La taille doit être plus petite que 50 caracters")]
         [Remote(action: "VerifyName", controller: "Customers", AdditionalFields ="Lastname, Id", ErrorMessage = "Le client exsiste déja")]
         public string Firstname { get; set; }
+
         [DisplayName("Adresse")]
         [StringLength(50, ErrorMessage = "La taille doit être plus petite que 50 caracters")]
         public string Address { get; set; }
-        [DisplayName("Téléphone")]
-        [Phone(ErrorMessage = "Le numero n'est pas valide")]
+
+        [DisplayName("Téléphone (Prefix Obligatoire")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\+?)(\d{2,4})(\s?)(\-?)((\(0\))?)(\s?)(\d{2})(\s?)(\-?)(\d{3})(\s?)(\-?)(\d{2})(\s?)(\-?)(\d{2})", ErrorMessage = "Le numero n'est pas valide")]
         public string Phone { get; set; }
+
         [DisplayName("Email")]
         [EmailAddress(ErrorMessage = "Le format email n'est pas valide")]
         [StringLength(50, ErrorMessage = "La taille doit être plus petite que 50 caracters")]
         public string Email { get; set; }
-        [DisplayName("Mobile")]
-        [Phone(ErrorMessage = "Le numero n'est pas valide")]
+
+        [DisplayName("Mobile (Prefix Obligatoire)")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\+?)(\d{2,4})(\s?)(\-?)((\(0\))?)(\s?)(\d{2})(\s?)(\-?)(\d{3})(\s?)(\-?)(\d{2})(\s?)(\-?)(\d{2})", ErrorMessage = "Le numero n'est pas valable")]
         public string Mobile { get; set; }
 
         [DisplayName("Ville")]
