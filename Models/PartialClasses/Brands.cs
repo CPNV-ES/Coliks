948cd894b6ab870cc91e6445e3f58167483f89f1
GIORDANO Antonio <Antonio.giordano17@gmail.com>
2019-04-03 15:57:54 +0200

    Creation de la page Brand et affichage des statistiques dans les détails d'une marque

---
+++ Models/PartialClasses/Brands.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace coliks.Models
+{
+    [ModelMetadataType(typeof(BrandsMetaData))]
+    public partial class Brands
+    {
+    }
+
+    public class BrandsMetaData
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nom de la marque")]
+        public string Brandname { get; set; }
+    }
+}
---
+++ Models/PartialClasses/Customers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace coliks.Models
+{
+    [ModelMetadataType(typeof(CustomersMetaData))]
+    public partial class Customers
+    {
+    }
+
+    public class CustomersMetaData
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nom")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "Prénom")]
+        public string Firstname { get; set; }
+
+        public string Address { get; set; }
+        public int? CityId { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+        public virtual Cities City { get; set; }
+    }
+}
